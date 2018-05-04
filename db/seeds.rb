e553652476a775af2f32a9a4196a6a54ba1ae086
Eric Grenier <eric@legrenier.org>
2018-05-04 22:04:16 +0200

    Ajout données playlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Playlist.create(name: 'Karaoké de Mureils du 26 mai 2018') 
